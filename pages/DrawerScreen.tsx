62b0eb6af4f5fb83513cd36444e69a3a7188f2db
Robin <Robin_Nygren@outlook.com>
2024-11-19 13:31:36 +0100

    Add Drawer navigation and update dependencies

--- pages/DrawerScreen.tsx
+++ pages/DrawerScreen.tsx
@@ -1,7 +1,19 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { View, Text } from "react-native";
+import { RootStackTypes } from "../types/types";
+import { useNavigation } from "@react-navigation/native";
+import { Button } from "@react-navigation/elements";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
 export const Drawer = () => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackTypes, "Drawer">>();
+
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={() => navigation.getParent("LeftDrawer").openDrawer()}>
+        Open Left Drawer
+      </Button>
       <Text>Test</Text>
     </View>
   );
--- types/types.ts
+++ types/types.ts
@@ -1,6 +1,8 @@
 export type RootStackTypes = {
   Home: undefined;
   Test: undefined;
+  Drawer: undefined;
+  LeftDrawer: undefined;
 };
 
 export type AuthContextType = {
