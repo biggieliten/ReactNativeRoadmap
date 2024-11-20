329c4d8b5db79383c9fbff83755af0ec066bfe10
pjkbsn <philip.jkbsn@gmail.com>
2024-11-20 13:51:18 +0100

    Camelcase

--- pages/Homescreen.tsx
+++ pages/Homescreen.tsx
@@ -3,7 +3,7 @@ import { Text, View, Button } from "react-native";
 export const HomeScreen = ({ navigation }: any) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Text>Home screen</Text>
       <Button title="Open Drawer" onPress={() => navigation.openDrawer()} />
     </View>
   );
